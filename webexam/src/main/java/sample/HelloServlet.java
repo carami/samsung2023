de69e503e4c65d0190d9eaa4ce9fc67b97da7a17
carami <carami@nate.com>
2023-03-08 15:08:49 +0900

    webexam 추가

--- webexam/src/main/java/sample/HelloServlet.java
+++ webexam/src/main/java/sample/HelloServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet({ "/haha", "/hoho" , "/test" })
+//@WebServlet({ "/haha", "/hoho" , "/test" })
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,21 +27,26 @@ public class HelloServlet extends HttpServlet {
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		System.out.println("init 실행");
 	}
 
 	/**
 	 * @see Servlet#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("destory !!!실행");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet 호출!!");
 	}
 
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("HelloServlet service!!! 실행!!");
-	}
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("HelloServlet service!!! 실행!!");
+//	}
 
 }
